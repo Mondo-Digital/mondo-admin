7a808ec3aa4499604864f718882e74d68a8ba00f
Daniel Luz <danielluz1995@gmail.com>
2020-07-10 22:45:24 -0300

    Add client token api validation.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,17 @@
 module Api
   module V1
     class ApiController < ActionController::Base
-      def validate_api_header_token!
+      protect_from_forgery with: :null_session
+      before_action :validate_api_token!
 
+      def validate_api_token!
+        head :forbidden unless valid_client_token?
+      end
+
+      private
+
+      def valid_client_token?
+        request.headers['HTTP_CLIENT_TOKEN'] == ENV.fetch('CLIENT_TOKEN')
       end
     end
   end
---
+++ spec/controllers/api/v1/api_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ApiController do
+  controller do
+    before_action :validate_api_token!
+
+    def index
+      render json: {}, status: :ok
+    end
+  end
+
+  let(:valid_token) { 'S3CR3TT0K3N' }
+
+  context 'when client token is valid' do
+    it 'checks for a valid token' do
+      stub_const('ENV', 'CLIENT_TOKEN' => valid_token)
+
+      request.headers['HTTP_CLIENT_TOKEN'] = valid_token
+      get :index,
+          params: {}
+
+      expect(response).to be_successful
+    end
+  end
+
+  context 'when client token is not valid' do
+    it 'checks for a valid token' do
+      stub_const('ENV', 'CLIENT_TOKEN' => valid_token)
+
+      request.headers['HTTP_CLIENT_TOKEN'] = 'some-token'
+      get :index,
+          params: {}
+
+      expect(response.status).to eq(403)
+    end
+  end
+end
