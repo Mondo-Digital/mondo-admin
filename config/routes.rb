3d2b957cb99484bc406cdf04f0ebd6fc627765ba
Daniel Luz <danielluz1995@gmail.com>
2020-07-11 21:25:28 -0300

    Initial user listing page setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'welcome#index'
 
   namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
       resources :users, only: [:create]
     end
   end
+
+  namespace 'app' do
+    resources :users, only: [:index]
+  end
+
+  root to: 'app/users#index'
 end
