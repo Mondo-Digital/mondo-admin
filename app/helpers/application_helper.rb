a9ccf092aff051ad0246aabf671a032648815b72
Daniel Luz <danielluz1995@gmail.com>
2020-07-14 00:17:23 -0300

    Check active link helper based on path start.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
   def active_link_to(name = nil, options = {}, html_options = {}, &block)
+    current_path_match(options)
     active_class = html_options[:active] || "active"
     html_options.delete(:active)
-    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(options)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_path_match(options)
     link_to(name, options, html_options, &block)
+  end
+
+  private
+
+  def current_path_match(options)
+    current_page?(options) || request.path.start_with?(options)
   end
 end
